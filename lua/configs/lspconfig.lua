c714940c7fdaffdcd225af6952fea9270e3a0ace
Ben Vaughan <b.f.vaughan2026@gmail.com>
2025-12-06 12:50:57 -0500

    2 - added lua lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "clangd" }
+local servers = { "html", "cssls", "clangd", "stylua" }
 vim.lsp.enable(servers)
 
 -- to configure lsps further read :h vim.lsp.config
